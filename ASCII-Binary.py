d635ffb900cd2f2db0effb3c510bec60c495319d
hiddendestroyer1945 <hiddendestroyer1945@gmail.com>
2025-12-27 13:06:21 -0600

    ASSCII-Binary.py created by py-develop branch. It's for explaining the normal text's binary form.

---
+++ ASCII-Binary.py
@@ -0,0 +1,194 @@
+data = input("Enter the data: ")
+
+if data == "0":
+    print("00110000")
+elif data == "1":
+    print("00110001")
+elif data == "2":
+    print("00110010")
+elif data == "3":
+    print("00110011")
+elif data == "4":
+    print("00110100")
+elif data == "5":
+    print("00110101")
+elif data == "6":
+    print("00110110")
+elif data == "7":
+    print("00110111")
+elif data == "8":
+    print("00111000")
+elif data == "9":
+    print("00111001")
+elif data == "A":
+    print("01000001")
+elif data == "B":
+    print("01000010")
+elif data == "C":
+    print("01000011")
+elif data == "D":
+    print("01000100")
+elif data == "E":
+    print("01000101")
+elif data == "F":
+    print("01000110")
+elif data == "G":
+    print("01000111")
+elif data == "H":
+    print("01001000")
+elif data == "I":
+    print("01001001")
+elif data == "J":
+    print("01001010")
+elif data == "K":
+    print("01001011")
+elif data == "L":
+    print("01001100")
+elif data == "M":
+    print("01001101")
+elif data == "N":
+    print("01001110")
+elif data == "O":
+    print("01001111")
+elif data == "P":
+    print("01010000")
+elif data == "Q":
+    print("01010001")
+elif data == "R":
+    print("01010010")
+elif data == "S":
+    print("01010011")
+elif data == "T":
+    print("01010100")
+elif data == "U":
+    print("01010101")
+elif data == "V":
+    print("01010110")
+elif data == "W":
+    print("01010111")
+elif data == "X":
+    print("01011000")
+elif data == "Y":
+    print("01011001")
+elif data == "Z":
+    print("01011010")
+elif data == "a":
+    print("01100001")
+elif data == "b":
+    print("01100010")
+elif data == "c":
+    print("01100011")
+elif data == "d":
+    print("01100100")
+elif data == "e":
+    print("01100101")
+elif data == "f":
+    print("01100110")
+elif data == "g":
+    print("01100111")
+elif data == "h":
+    print("01101000")
+elif data == "i":
+    print("01101001")
+elif data == "j":
+    print("01101010")
+elif data == "k":
+    print("01101011")
+elif data == "l":
+    print("01101100")
+elif data == "m":
+    print("01101101")
+elif data == "n":
+    print("01101110")
+elif data == "o":
+    print("01101111")
+elif data == "p":
+    print("01110000")
+elif data == "q":
+    print("01110001")
+elif data == "r":
+    print("01110010")
+elif data == "s":
+    print("01110011")
+elif data == "t":
+    print("01110100")
+elif data == "u":
+    print("01110101")
+elif data == "v":
+    print("01110110")
+elif data == "w":
+    print("01110111")
+elif data == "x":
+    print("01111000")
+elif data == "y":
+    print("01111001")
+elif data == "z":
+    print("01111010")
+elif data == "{":
+    print("01111011")
+elif data == "}":
+    print("01111101")
+elif data == "|":
+    print("01111100")
+elif data == "\\":
+    print("01011100")    
+elif data == "~":
+    print("01111110")
+elif data == " ":
+    print("00100000")
+elif data == "!":
+    print("00100001")
+elif data == "#":
+    print("00100011")
+elif data == "$":
+    print("00100100")
+elif data == "%":
+    print("00100101")
+elif data == "&":
+    print("00100110")
+elif data == "'":
+    print("00100111")
+elif data == "(":
+    print("00101000")
+elif data == ")":
+    print("00101001")
+elif data == "*":
+    print("00101010")
+elif data == "+":
+    print("00101011")
+elif data == "-":
+    print("00101101")
+elif data == "/":
+    print("00101111")
+elif data == ":":
+    print("00111010")
+elif data == ";":    
+    print("00111011")
+elif data == '"':
+    print("00100010")
+elif data == "<":
+    print("00111100")
+elif data == "=":
+    print("00111101")
+elif data == ">":
+    print("00111110")
+elif data == ".":
+    print("00101110")
+elif data == ",":
+    print("00101100")
+elif data == "?":
+    print("00111111")
+elif data == "@":
+    print("01000000")
+elif data == "[":
+    print("01011011")
+elif data == "]":
+    print("01011101")
+elif data == "^":
+    print("01011110")
+elif data == "_":
+    print("01011111")
+elif data == "`":
+    print("01100000")
+else:
+    print("Invalid data")
